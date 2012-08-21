938543e5779e5eea68ef49a80320c272445093bf
blackanger <blackanger.z@gmail.com>
2012-08-21 14:34:00 +0800

    add uniq name method from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < RedisRecord::Base
+ 
 
+  def uniq_name
+  	name = generate_user_name
+  	exist?(name) ? generate_user_name : name
+  end
+
+protected
+
+  def generate_user_name
+    pool = ('a'..'z').to_a + (1..9).to_a
+    name = (0..5).map{ pool[rand(pool.length)]}.join
+    return name
+  end
+
+  def exist?(name)
+    #TODO
+    false
+  end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -1,1 +1,15 @@
 $: << "../../app" << "./app" << "../../lib" << "./lib"
+
+# models
+require 'models/redis_record'
+require 'models/user'
+
+# controllers
+
+
+# lib
+
+# constant define
+
+
+# Bid
