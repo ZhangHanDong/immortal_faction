cd7785b282f6a25cf2be8932768126f7bbce0e5b
blackanger <blackanger.z@gmail.com>
2012-09-06 12:23:20 +0800

    modify reg

--- app/models/reg_attr.rb
+++ app/models/reg_attr.rb
@@ -4,7 +4,7 @@ class RegAttr < RedisRecord::Base
   
   attr_accessor :attribute, :user_id
 
-  def create(options={})
+  def self.create(options={})
     if options[:user_id] && options[:attribute]
       RegAttr.hmset("imf:reg:#{options[:user_id]}", "user_id", options[:user_id], "attribute", options[:attribute])
       return true
