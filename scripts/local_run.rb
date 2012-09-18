e28cf0540ff2404fc805a75814e4e33d8160238f
blackanger <blackanger.z@gmail.com>
2012-09-18 14:44:26 +0800

    modify scripts

---
+++ scripts/local_run.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+puts "------> starting redis"
+%x|
+  redis-server /usr/local/etc/redis.conf
+  redis-server /usr/local/etc/redis_6377.conf
+  redis-server /usr/local/etc/redis_6378.conf
+|
+puts "-------> redis prepared!"
+
+puts "---------> starting goliath server "
+servers = {:sign_up => 9000, :password => 9001, :login => 9002, :reg_choice => 9003, :echo_server => 9004}
+servers.each do |k, v|
+  %x| touch ./logs/imf_#{v}.log_stdout.log|
+  %x| chmod 755 ./logs/imf_#{v}.log_stdout.log|
+  path = %x|pwd|.chomp
+  %x| ruby config/route.rb #{k} -e prod -p #{v} -l #{path}/logs/imf_#{v}.log -P #{path}/logs/goliath.pid -d |
+end
+puts "----------> ok, goliath prepared!"
+
+puts "-----> starting haproxy server..."
+
+%x| sudo /usr/local/sbin/haproxy -f /usr/local/Cellar/haproxy/1.4.21/haproxy.cfg |
+
+puts "-------> ok, haproxy prepared for you!"
--- scripts/restart.rb
+++ scripts/restart.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby
-puts "----------> restarting Goliath Server ..."
+puts "----------> killed Goliath Server ..."
 
 pids = %x[ps aux|grep route.rb|grep -v grep|awk '{print $2}'|xargs].chomp.split
 pids.each do |pid|
   %x|kill -9 #{pid.to_i}| 
 end
 
-%x[ ruby scripts/run.rb]
+puts "------ killde haproxy server ..."
+ha_pids = %x[ps aux|grep haproxy|grep -v grep|awk '{print $2}'|xargs].chomp
+%x|sudo kill -9 #{ha_pids.to_i}|
+
+puts "------> starting goliath and haproxy server ..."
+%x[ ruby scripts/local_run.rb]
 
 puts "--------> restared success!"
 
