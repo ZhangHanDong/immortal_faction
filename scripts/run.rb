e5eb3512a4a9bdcec93a9982d1ecdf424c9ff676
blackanger <blackanger.z@gmail.com>
2012-09-19 15:16:10 +0800

    modify

--- scripts/run.rb
+++ scripts/run.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 puts "---------> starting goliath server "
-servers = {:sign_up => 9000, :password => 9001, :login => 9002, :reg_choice => 9003, :echo_server => 9004}
+servers = {:sign_up => 9000, :password => 9001, :login => 9002, :reg_choice => 9003}
 servers.each do |k, v|
   %x| touch ./logs/imf_#{v}.log_stdout.log|
   %x| chmod 755 ./logs/imf_#{v}.log_stdout.log|
